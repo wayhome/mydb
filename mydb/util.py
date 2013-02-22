047ed27c06637da516a0cc97ccacf3fa98c1dcd0
YoungKing <yanckin@gmail.com>
2013-02-22 16:26:20 +0800

    fix import module

--- mydb/util.py
+++ mydb/util.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
+def _resolve_name(name, package, level):
+    """Return the absolute name of the module to be imported."""
+    if not hasattr(package, 'rindex'):
+        raise ValueError("'package' not set to a string")
+    dot = len(package)
+    for x in xrange(level, 1, -1):
+        try:
+            dot = package.rindex('.', 0, dot)
+        except ValueError:
+            raise ValueError("attempted relative import beyond top-level "
+                              "package")
+    return "%s.%s" % (package[:dot], name)
+
 
 
 def import_module(name, package=None):
